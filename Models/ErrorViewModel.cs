300d4e4e178bbf9f4c270f1afb0ff65fcd0cd50a
Rasoul Nikmehr <rasoul.nikmehr@gmail.com>
2024-02-14 21:55:24 -0500

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloAzureWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
